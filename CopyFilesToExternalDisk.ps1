86dcc0d13a0700398175c52bf1d7637ee2c0b853
acotarlan <alexcotarlan@gmail.com>
2024-08-15 09:40:06 -0400

    CopyFilesToExternalDisk.ps1

--- CopyFilesToExternalDisk.ps1
+++ CopyFilesToExternalDisk.ps1
@@ -1,4 +1,9 @@
 ﻿
+#CopyFilesToExternalDisk.ps1
+#Copyright 2024 Alex Cotarlan
+
+#This product includes software developed at groktrain.com 
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
